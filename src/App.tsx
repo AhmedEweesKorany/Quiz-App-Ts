ebf29e62b7ee0f8047e4f9c5c8b5bd91cd038b29
AhmedEweesKorany <ahmedewaisvi34@gmail.com>
2025-01-02 23:53:21 +0200

    remove useEffect from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, {  useState } from "react"
 import QuestionCard from "./Components/QuestionCard"
 import { Diff, fetchQuizData, QuestionState } from "./Api"
 import { GlobalStyle, Wrapper } from "./App.style";
